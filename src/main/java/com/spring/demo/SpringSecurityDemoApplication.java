039385d592e133a5b1dae400097a063df327cffd
Yogesh Deshmukh <sampoorna.mahanti@tcs.com>
2020-09-04 08:55:00 +0530

    Added SQL properties and some code

--- src/main/java/com/spring/demo/SpringSecurityDemoApplication.java
+++ src/main/java/com/spring/demo/SpringSecurityDemoApplication.java
@@ -3,6 +3,8 @@ package com.spring.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
--- src/main/java/com/spring/demo/model/User.java
+++ src/main/java/com/spring/demo/model/User.java
@@ -2,21 +2,21 @@ package com.spring.demo.model;/*
 @Author : Yogesh Deshmukh
 */
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class User {
     private Long id;
     private String username;
     private String password;
-
+    private Set<Authorities> authorities = new HashSet<>();
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
     }
+
 
     public void setId(Long id) {
         this.id = id;
@@ -36,5 +36,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    public Set<Authorities> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authorities> authorities) {
+        this.authorities = authorities;
     }
 }
